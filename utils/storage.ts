157a12b440de493e22761a30b6d98470183df073
seekinglight233 <690163223@qq.com>
2023-05-06 10:22:15 +0800

    feat: Storage util

---
+++ utils/storage.ts
@@ -0,0 +1,61 @@
+import type { Bookmark } from "~model/bookmark";
+
+class Storage {
+  private storage: chrome.storage.StorageArea;
+
+  constructor(isDevMode: boolean) {
+    this.storage = isDevMode ? chrome.storage.local : chrome.storage.sync;
+  }
+
+  get(key: string): Promise<Bookmark> {
+    return new Promise((resolve, reject) => {
+      this.storage.get(key, (result) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(result[key]);
+        }
+      });
+    });
+  }
+
+  getAll(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.storage.get(null, (result) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  set(key: string, value: Bookmark): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.storage.set({ [key]: value }, () => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  clear(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.storage.clear(() => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+const isDevMode = process.env.NODE_ENV === 'development';
+const storage = new Storage(isDevMode);
+export default storage;
