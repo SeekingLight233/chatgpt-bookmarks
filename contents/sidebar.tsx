54cf29afc7c13c8e0f21b0ce18e86cd56f310277
seekinglight233 <690163223@qq.com>
2023-05-26 23:03:52 +0800

    feat: render all data by group

--- contents/sidebar.tsx
+++ contents/sidebar.tsx
@@ -70,7 +70,6 @@ const Sidebar = () => {
     document.body.classList.toggle("bookmark-sidebar-show", isOpen)
   }, [isOpen])
 
-
   const { run: runSetActiveId } = useThrottleFn(
     () => {
       const newActiveId = getActiveId(allBookmarks)
@@ -92,7 +91,7 @@ const Sidebar = () => {
 
     return () => {
       if (scrollDom) {
-        scrollDom.removeEventListener("scroll", () => { })
+        scrollDom.removeEventListener("scroll", () => {})
       }
     }
   }, [scrollDom])
@@ -105,13 +104,32 @@ const Sidebar = () => {
     return list.filter((bookmark) => bookmark.title.includes(searchValue))
   })
 
-  const renderBookmarks = () => {
-    const list =
-      curTab === "all" ? allBookmarks : filterByCurSessionId(allBookmarks)
-    const filteredList = filterBySearchValue(list)
+  const renderAllBookmarks = (list: Bookmark[]) => {
+    const groupedBookmarks = groupByDate(list)
+    return groupedBookmarks.map(({ label, data }, idx) => {
+      if (data.length === 0) return null
+      return (
+        <div key={idx}>
+          <div style={styles.dateLabel}>{label}</div>
+          {data.map((bookmark, idx) => (
+            <BookmarkItem
+              key={idx}
+              onClick={scrollIntoBookmark}
+              onEdit={(bookmark) => {
+                bookmarkStore.onEdit(bookmark)
+              }}
+              active={activeId === bookmark.bookmarkId}
+              onDelete={bookmarkStore.onDelete}
+              {...bookmark}
+            />
+          ))}
+        </div>
+      )
+    })
+  }
 
-    if (filteredList.length === 0) return <Empty></Empty>
-    return filteredList.map((bookmark, idx) => (
+  const renderCurrentBookmarks = (list: Bookmark[]) => {
+    return list.map((bookmark, idx) => (
       <BookmarkItem
         key={idx}
         onClick={scrollIntoBookmark}
@@ -123,6 +141,14 @@ const Sidebar = () => {
         {...bookmark}
       />
     ))
+  }
+
+  const renderBookmarks = () => {
+    const filterValuedList = filterBySearchValue(allBookmarks)
+    if (filterValuedList.length === 0) return <Empty></Empty>
+    return curTab === "current"
+      ? renderCurrentBookmarks(filterValuedList)
+      : renderAllBookmarks(filterValuedList)
   }
 
   const siderbarWidth = getSiderbarWidth() - 4
@@ -209,6 +235,72 @@ function setActiveIdOnUrl(activeId: number) {
   history.replaceState(null, null, url.join("#"))
 }
 
+type GroupedBookmarks = { label: string; data: Bookmark[] }[]
+
+function groupByDate(bookmarks: Bookmark[]): GroupedBookmarks {
+  const now = new Date()
+  const oneDay = 24 * 60 * 60 * 1000 // milliseconds in a day
+  const sevenDays = 7 * oneDay
+  const thirtyDays = 30 * oneDay
+
+  const today = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate()
+  ).getTime()
+  const yesterday = today - oneDay
+  const previous7Days = today - sevenDays
+  const previous30Days = today - thirtyDays
+
+  let grouped: GroupedBookmarks = [
+    { label: "Today", data: [] },
+    { label: "Yesterday", data: [] },
+    { label: "Previous 7 Days", data: [] },
+    { label: "Previous 30 Days", data: [] }
+  ]
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+
+  for (let bookmark of bookmarks) {
+    if (bookmark.createUnix >= today) {
+      grouped[0].data.push(bookmark)
+    } else if (bookmark.createUnix >= yesterday) {
+      grouped[1].data.push(bookmark)
+    } else if (bookmark.createUnix >= previous7Days) {
+      grouped[2].data.push(bookmark)
+    } else if (bookmark.createUnix >= previous30Days) {
+      grouped[3].data.push(bookmark)
+    } else {
+      // Group by month for older bookmarks
+      const date = new Date(bookmark.createUnix)
+      const yearMonth = `${date.getFullYear()}-${monthNames[date.getMonth()]}` // month is 0-indexed
+
+      let monthGroup = grouped.find((group) => group.label === yearMonth)
+      if (!monthGroup) {
+        monthGroup = { label: yearMonth, data: [] }
+        grouped.push(monthGroup)
+      }
+
+      monthGroup.data.push(bookmark)
+    }
+  }
+
+  return grouped
+}
+
 const styles = createStyles({
   toggleBtn: {
     position: "fixed",
@@ -227,13 +319,22 @@ const styles = createStyles({
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    paddingTop: 26
+    paddingTop: 10
   },
   scrollArea: {
     height: "90vh",
     width: "100%",
     paddingBottom: "20%",
     overflow: "scroll"
+  },
+  dateLabel: {
+    fontSize: 12,
+    color: theme.groupLabelColor,
+    height: 30,
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: 24,
+    marginBottom: -4
   }
 })
 
