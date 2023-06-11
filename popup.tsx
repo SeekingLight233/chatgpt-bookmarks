7199603d9dd57a7c21225aeadfb13231dc04192f
seekinglight233 <690163223@qq.com>
2023-06-11 18:28:40 +0800

    feat: add popup

--- popup.tsx
+++ popup.tsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from "react"
 
+import { createStyles } from "~utils/base"
+import theme from "~utils/theme"
+
 function IndexPopup() {
-  const [data, setData] = useState("")
-
-  useEffect(() => {
-    console.log("popup")
-  })
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>chatgpt bookmarks is actived!111</h2>
+    <div style={styles.container}>
+      <span style={styles.text}>Chat-GPT bookmarks is actived!</span>
     </div>
   )
 }
+
+const styles = createStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    width: 280,
+    height: 20,
+    backgroundColor: "#f2f2f2", // 设置背景色
+    borderRadius: "5px", // 设置边框圆角
+    justifyContent: "center", // 水平居中
+    alignItems: "center", // 垂直居中
+    boxShadow: "0px 0px 5px rgba(0,0,0,0.2)", // 添加阴影
+    padding: "10px" // 添加内边距
+  },
+  text: {
+    fontSize: "14px", // 设置字体大小
+    color: "#333", // 设置字体颜色
+    fontWeight: "bold" // 设置字体粗细
+  }
+})
 
 export default IndexPopup
